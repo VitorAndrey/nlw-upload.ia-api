52ebce2fbc2a51ad7012e15ddd82165734053976
Vitor Andrey <vitor.andreylopess@gmail.com>
2023-09-12 22:05:47 -0300

    created completion route and added cors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
 
 import { uploadVideoRoute } from "./routes/upload-video";
 import { getAllProptsRoute } from "./routes/get-all-prompts";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 app.register(getAllProptsRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
+app.register(generateAICompletionRoute);
 
 app
   .listen({
